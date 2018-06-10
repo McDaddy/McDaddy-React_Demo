252a24b7ea8ddc7fd47f05ce0e7c69a631c55d85
chenweitao <chenweitao@chenweitaodeiMac.local>
2018-06-10 21:47:18 +0800

    add download function

--- app/js/public/header.js
+++ app/js/public/header.js
@@ -8,13 +8,34 @@ class Header extends React.Component {
     super();
   }
 
+  download = () => {
+    fetch('http://localhost:3000', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+
+      }
+    }).then(response => {
+      // console.log(response.blob());
+      return response.blob();
+    }).then(data => {
+      console.log(data);
+      let a = document.createElement('a');
+      let url = window.URL.createObjectURL(data);
+      let filename = 'my-excel.xlsx';
+      a.href = url;
+      a.download = filename;
+      a.click();
+    })
+  }
+
   render () {
     return (
       <div styleName='header'>
         <span styleName='left'>
           <em>注册</em>&nbsp;|&nbsp;<em>登录</em>
         </span>
-        <btn styleName='btnDownload'>APP下载</btn>
+        <button styleName='btnDownload' onClick={this.download}>APP下载</button>
       </div>
     );
   }
